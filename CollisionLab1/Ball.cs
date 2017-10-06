734ebc095092a7afbfa3d7049d972882e939bbbc
Jacky Yao <blackdragon.yao@gmail.com>
2017-10-06 14:13:32 -0400

    update program to encompass x and y coordinates

--- CollisionLab1/Ball.cs
+++ CollisionLab1/Ball.cs
@@ -4,19 +4,20 @@ namespace CollisionLab1
 {
     public class Ball
     {
-        public double v, posx, posy,  a, radius, spring, mass, rotation, angle ;
+        public double vx,vy, posx, posy,  ax, ay, radius, spring, mass, rotation, angle ;
         public Boolean clockwise;
         public Ball(double px,double py, double v1, double a1, double r, double s, double m, double rotate, double ang, Boolean yes)
         {
-            this.v = v1;
+            this.vx = v1*Math.Cos(ang);
+            this.vy = v1 * Math.Sin(ang);
             this.posx = px;
             this.posy = py;
-            this.a = a1;
+            this.ax = a1*Math.Cos(ang);
+            this.ay = a1 * Math.Sin(ang);
             this.radius = r;
             this.spring = s;
             this.mass = m;
             this.rotation = rotate;
-            this.angle = ang;
             this.clockwise = yes;
         }
         
